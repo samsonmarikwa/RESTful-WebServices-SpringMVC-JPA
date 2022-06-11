510cc5c15fe1e85a3e78a68411c15934e6a2a1a1
Samson Marikwa <samsonmarikwa@outlook.com>
2022-06-11 13:05:34 -0400

    Make use of the UserRest and UserDto models in RestController

--- mobile-app-ws/src/main/java/com/samsonmarikwa/appws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/samsonmarikwa/appws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.samsonmarikwa.appws.ui.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.samsonmarikwa.appws.shared.dto.UserDto;
 import com.samsonmarikwa.appws.ui.model.request.UserDetailsRequestModel;
 import com.samsonmarikwa.appws.ui.model.response.UserRest;
 
@@ -23,7 +25,15 @@ public class UserController {
 	@PostMapping
 	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
 		
-		return null;
+		UserRest returnValue = new UserRest();
+		
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails, userDto);
+		
+		UserDto createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+		
+		return returnValue;
 	}
 	
 	@PutMapping
