949e6e41d1ddbdf5977a1f1dc1780fc2627d51cc
Samson Marikwa <samsonmarikwa@outlook.com>
2022-06-11 20:32:35 -0400

    Check if user already exists

--- mobile-app-ws/src/main/java/com/samsonmarikwa/appws/io/entity/UserEntity.java
+++ mobile-app-ws/src/main/java/com/samsonmarikwa/appws/io/entity/UserEntity.java
@@ -16,7 +16,7 @@ public class UserEntity implements Serializable {
 	@GeneratedValue
 	private long id;
 	
-	@Column(nullable=false)
+	@Column(nullable=false, unique=true)
 	private String userId;
 	
 	@Column(nullable=false, length=50)
@@ -25,7 +25,7 @@ public class UserEntity implements Serializable {
 	@Column(nullable=false, length=50)
 	private String lastName;
 	
-	@Column(nullable=false, length=120)
+	@Column(nullable=false, length=120, unique=true)
 	private String email;
 	
 	@Column(nullable=false)
--- mobile-app-ws/src/main/java/com/samsonmarikwa/appws/service/UserService.java
+++ mobile-app-ws/src/main/java/com/samsonmarikwa/appws/service/UserService.java
@@ -1,10 +1,7 @@
 package com.samsonmarikwa.appws.service;
 
-import org.springframework.stereotype.Service;
-
 import com.samsonmarikwa.appws.shared.dto.UserDto;
 
-@Service
 public interface UserService {
 	
 	UserDto createUser(UserDto user);
