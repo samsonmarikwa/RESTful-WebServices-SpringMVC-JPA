3a172e23cc793b1a14694bb9a462a5c20d65fef1
Samson Marikwa <samsonmarikwa@outlook.com>
2022-06-14 20:23:48 -0400

    Customize User Authentication URL

--- mobile-app-ws/src/main/java/com/samsonmarikwa/appws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/samsonmarikwa/appws/security/WebSecurity.java
@@ -27,12 +27,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
 			.anyRequest().authenticated()
 			.and()
-			.addFilter(new AuthenticationFilter(authenticationManager()));
+			.addFilter(getAuthenticationFilter());
 	}
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	public AuthenticationFilter getAuthenticationFilter() throws Exception {
+		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+		filter.setFilterProcessesUrl("/users/login");
+		return filter;
 	}
 
 }
