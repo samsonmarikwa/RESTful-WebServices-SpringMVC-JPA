a7c7437328098564b55c57f6d867d589406cb374
Samson Marikwa <samsonmarikwa@outlook.com>
2022-06-24 17:02:44 -0400

    Add password-reset-request path as public so it does not require authentication

--- mobile-app-ws/src/main/java/com/samsonmarikwa/appws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/samsonmarikwa/appws/security/WebSecurity.java
@@ -27,6 +27,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		http.csrf().disable().authorizeRequests()
 			.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
 			.antMatchers(HttpMethod.GET, SecurityConstants.VERIFICATION_EMAIL_URL).permitAll()
+			.antMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_REQUEST_URL).permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.addFilter(getAuthenticationFilter())
