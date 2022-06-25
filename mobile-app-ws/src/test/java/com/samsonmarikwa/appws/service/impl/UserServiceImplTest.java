c4c7ff22c417f807c49f3a6cca0af0707497b942
Samson Marikwa <samsonmarikwa@outlook.com>
2022-06-25 19:23:22 -0400

    JUnit5 - Asserting successful method execution

---
+++ mobile-app-ws/src/test/java/com/samsonmarikwa/appws/service/impl/UserServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.samsonmarikwa.appws.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.samsonmarikwa.appws.io.entity.UserEntity;
+import com.samsonmarikwa.appws.repository.UserRepository;
+import com.samsonmarikwa.appws.shared.dto.UserDto;
+
+class UserServiceImplTest {
+	
+	@InjectMocks
+	UserServiceImpl userService;
+	
+	@Mock
+	UserRepository userRepository;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testGetUser() {
+		
+		UserEntity userEntity = new UserEntity();
+		// stub - fake object
+		userEntity.setId(1L);
+		userEntity.setUserId("abcd-efgh");
+		userEntity.setFirstName("Samson");
+		userEntity.setLastName("Marikwa");
+		userEntity.setEmail("sammari4249@gmail.com");
+		
+		when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
+		
+		UserDto userDto = userService.getUser("test@test.com");
+		
+		assertNotNull(userDto);
+		assertEquals("Samson", userDto.getFirstName());
+		assertEquals("Marikwa", userDto.getLastName());
+		
+	}
+
+}
